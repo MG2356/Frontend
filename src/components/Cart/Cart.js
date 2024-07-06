b009056ce6e8f4dbd03d0ccb416d4d5b41685a3e
Munish Goel <98271284+MG2356@users.noreply.github.com>
2024-07-06 21:35:59 +0530

    .

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,15 +3,18 @@ import StripeCheckout from "react-stripe-checkout";
 import { cartContext } from "../../GlobalState/CartContext";
 import { decrement, deleteProduct, increment } from "../../actions";
 import Icon from 'awesome-react-icons';
-import '../Product/Product.css'
+import '../Product/Product.css';
 
 const Cart = () => {
-  const { shoppingCart, dispatch, qty, totalPrice } = useContext(cartContext);
-  console.log(shoppingCart);
-  console.log("totalPrice: " + totalPrice);
+  const { shoppingCart, dispatch, qty } = useContext(cartContext);
+
+  // Calculate total price
+  const totalPrice = shoppingCart.reduce((acc, product) => acc + product.productPrice * product.qty, 0);
+
   const handleToken = (token) => {
     console.log("Hi");
   };
+
   return (
     <div className="container">
       <h1 className="main-heading">Shopping Cart</h1>
@@ -29,15 +32,9 @@ const Cart = () => {
               <div className="prod-name">{product.productName}</div>
               <div className="prod-price">${product.productPrice}.00</div>
               <div className="prod-cta">
-              <Icon name="plus"onClick={() => dispatch(increment(product.id))}
- />
-          
+                <Icon name="plus" onClick={() => dispatch(increment(product._id))} />
                 <span className="cart-quantity">{product.qty}</span>
-                <Icon name="minus"onClick={() => dispatch(decrement(product.id))}/>
-                {/* <i
-                  className="fas fa-minus"
-                  onClick={() => dispatch(decrement(product.id))}
-                ></i> */}
+                <Icon name="minus" onClick={() => dispatch(decrement(product._id))} />
               </div>
               <div className="total-prod-price">
                 ${product.productPrice * product.qty}
@@ -55,17 +52,17 @@ const Cart = () => {
         )}
       </div>
       {shoppingCart.length > 0 ? (
-        <div className="cart-summry">
+        <div className="cart-summary">
           <div className="summary">
-            <h3>Cart Summry</h3>
+            <h3>Cart Summary</h3>
             <div className="pay-total-items">
               <div className="items">Total Items</div>
               <div className="items-count">{qty}</div>
             </div>
             <div className="total-price-section">
               <div className="title">Total Price</div>
-              <div className="items-price">${totalPrice}.00</div>
-            </div>                  
+              <div className="items-price">${totalPrice.toFixed(2)}</div>
+            </div>
             <div className="stripe-section">
               <StripeCheckout
                 stripeKey="pk_test_51I2rmvG1SGhtxvtXhcM6ojfL5EknI0UiE5jqQedC0gFL6vAImspnRjomUXpZPgZuDRngSKBMSBG4GpibRC4crPZa00O1pb58yO"
